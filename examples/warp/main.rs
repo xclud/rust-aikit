b903443ebcfacf16d2a2a371eacc2dc0af113392
Mahdi <mkfard@outlook.com>
2024-03-02 16:11:07 +0330

    Warp image.

---
+++ examples/warp/main.rs
@@ -0,0 +1,36 @@
+use aikit::warp_into;
+use image::{Rgb, RgbImage, Rgba32FImage};
+use nalgebra::{Matrix3, Vector2};
+
+pub fn read_rgb_image(image_path: &str) -> Rgba32FImage {
+    let image = image::open(image_path).unwrap().to_rgba32f();
+
+    return image;
+}
+
+fn to_rgb8(image: Rgba32FImage) -> RgbImage {
+    let output = RgbImage::from_fn(image.width(), image.height(), |i, j| {
+        let px = image.get_pixel(i, j);
+        let p = px.0;
+
+        Rgb::<u8>([
+            (p[0] * 255.0) as u8,
+            (p[1] * 255.0) as u8,
+            (p[2] * 255.0) as u8,
+        ])
+    });
+
+    output
+}
+
+fn main() {
+    let input = read_rgb_image("t1.jpg");
+    let mut output = Rgba32FImage::new(256, 256);
+
+    let mut matrix = Matrix3::identity();
+
+    matrix = matrix.append_translation(&Vector2::new(-100f32, 0f32));
+
+    warp_into(input, matrix, &mut output);
+    _ = to_rgb8(output).save("saved_t1.jpg");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,43 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ops::Mul;
+
+use image::Rgba32FImage;
+use nalgebra::{Matrix3, Vector3};
+
+pub fn warp_into(input: Rgba32FImage, matrix: Matrix3<f32>, output: &mut Rgba32FImage) {
+    let inverse = matrix.try_inverse().unwrap();
+
+    let in_width = input.width();
+    let in_height = input.height();
+
+    let out_width = output.width();
+    let out_height = output.height();
+
+    for out_row in 0..out_width {
+        for out_col in 0..out_height {
+            let in_pixel = inverse.mul(Vector3::<f32>::new(out_row as f32, out_col as f32, 1f32));
+
+            let in_row = in_pixel[0] as i32;
+            let in_col = in_pixel[1] as i32;
+
+            if (0 <= in_row)
+                && (in_row < in_width as i32)
+                && (0 <= in_col)
+                && (in_col < in_height as i32)
+            {
+                let px = input.get_pixel(in_row as _, in_col as _);
+
+                output[(out_row, out_col)] = *px;
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
 }
